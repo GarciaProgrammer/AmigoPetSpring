41bdb4e63548a2a8d9ccf778e06057350c0b373c
Jefferson Möller <infomoller@gmail.com>
2021-02-05 22:02:14 -0300

    teste

--- amigopet/src/main/java/br/com/amigopet/controller/AnimalController.java
+++ amigopet/src/main/java/br/com/amigopet/controller/AnimalController.java
@@ -1,6 +1,5 @@
 package br.com.amigopet.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -18,16 +17,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import br.com.amigopet.controller.form.AnimalForm;
 import br.com.amigopet.controller.form.AtualizacaoAnimalForm;
 import br.com.amigopet.dto.AnimalDto;
 import br.com.amigopet.model.Animal;
-import br.com.amigopet.model.Usuario;
+
 import br.com.amigopet.repository.AnimalRepository;
 
-@CrossOrigin
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/animal")
 public class AnimalController {
@@ -35,24 +32,11 @@ public class AnimalController {
 	@Autowired
 	AnimalRepository animalRepository;
 
-	
-	
-//	@PostMapping("/cadastrar")
-//	@Transactional
-//	public void cadastrar(String nome, String tipo, String raca, int idade, String sexo, String descricao, Byte imagem,
-//			Usuario usuario, String cidade, String estado) {
-//		Animal animal = new Animal(nome, tipo, raca, idade, sexo, descricao, imagem, usuario, cidade, estado);
-//		animalRepository.save(animal);
-//	}
-	
-//	@PostMapping("/cadastrar")
-//	@Transactional
-//	public ResponseEntity<AnimalDto> cadastrar(@RequestBody AnimalForm form, UriComponentsBuilder uriBuilder) {
-//		Animal animal = form.converter();
-//		animalRepository.save(animal);
-//		URI uri = uriBuilder.path("/cadastrar/{id}").buildAndExpand(animal.getId()).toUri();
-//		return ResponseEntity.created(uri).body(new AnimalDto(animal));
-//	}
+	@PostMapping("/cadastrar")
+	public Animal cadastrar(@RequestBody Animal animal) {
+		return animalRepository.save(animal);
+
+	}
 
 	@GetMapping("/visualizar/{id}")
 	public AnimalDto visualizar(@PathVariable Long id) {
