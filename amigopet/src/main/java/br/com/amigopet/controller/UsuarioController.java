865ff8f4dd3e33a1591549539a3cc72353ab6c9e
Jefferson Möller <infomoller@gmail.com>
2021-02-07 18:29:19 -0300

    feito o cadastra,altera e busca da entidade Usuario

--- amigopet/src/main/java/br/com/amigopet/controller/UsuarioController.java
+++ amigopet/src/main/java/br/com/amigopet/controller/UsuarioController.java
@@ -1,19 +1,26 @@
 package br.com.amigopet.controller;
 
-import java.util.Optional;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.amigopet.controller.form.AtualizacaoUsuarioForm;
+import br.com.amigopet.dto.UsuarioComSenhaDto;
 import br.com.amigopet.dto.UsuarioDto;
 import br.com.amigopet.model.Usuario;
 import br.com.amigopet.repository.UsuarioRepository;
 
-@CrossOrigin
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
@@ -21,26 +28,24 @@ public class UsuarioController {
 	@Autowired
 	UsuarioRepository usuarioRepository;
 
-	@RequestMapping("/cadastra")
-	@PostMapping
-	public void cadastraUsuario(String nome, String email, String senha, String celular, String cidade, String estado) {
-		Usuario usuario = new Usuario(nome, email, senha, celular, cidade, estado);
-		usuarioRepository.save(usuario);
+	@PostMapping("/cadastrar")
+	public Usuario cadastrar(@RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
 	}
 
-	@RequestMapping("/visualiza")
-	@GetMapping
-	public UsuarioDto visualizaUsuario(Long id) {
-		Optional<Usuario> usuario = usuarioRepository.findById(id);
-		return UsuarioDto.converter(usuario);
+	@GetMapping("/visualizar/{id}")
+	@Transactional
+	public UsuarioDto visualizar(@PathVariable Long id) {
+		Usuario usuario = usuarioRepository.getOne(id);
+		return new UsuarioDto(usuario);
 	}
 
-	@RequestMapping("/altera")
-	@PostMapping
-	public void alteraUsuario(Long id, String nome, String email, String senha, String celular, String cidade,
-			String estado) {
-		Usuario usuario = new Usuario(id, nome, email, senha, celular, cidade, estado);
-		usuarioRepository.save(usuario);
+	@PutMapping("/alterar/{id}")
+	@Transactional
+	public ResponseEntity<UsuarioComSenhaDto> alterar(@PathVariable Long id,
+			@RequestBody @Valid AtualizacaoUsuarioForm form) {
+		Usuario usuario = form.atualizar(id, usuarioRepository);
+		return ResponseEntity.ok(new UsuarioComSenhaDto(usuario));
 	}
 
 }
